af94d61fc6556b86dc47264ad74a92b6b3bb8efb
Kadek Chresna <chresna@leson.id>
2020-08-25 12:33:09 +0700

    making case failed deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
+import React from "react";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-  className = 'App-link'
-  href = 'https://reactjs.org'
-  target = '_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
-        <p>Hi there!</p>
+        <p>Hello from the other side!</p>
       </header>
     </div>
   );
