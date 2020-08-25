d66ecffbac9d8117390eb1277215b31bf1b429b5
Kadek Chresna <chresna@leson.id>
2020-08-25 11:53:35 +0700

    add deployment config in travis config

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
-import {render} from '@testing-library/react';
-import React from 'react';
+import { render } from "@testing-library/react";
+import React from "react";
 
-import App from './App';
+import App from "./App";
 
-test('renders learn react link', () => {
-  const {getByText} = render(<App />);
-  // const linkElement = null;
-  const linkElement = getByText(/learn react/i);
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = null;
+  // const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
